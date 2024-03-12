b9d1d464c2d98d83f44ddc70da11cf794df27ee9
caffeinathan <nkrogowski+github1@gmail.com>
2024-03-12 02:38:15 -0600

    add Flowchart.Navigator as ersatz Iterator

--- src/main/java/org/comit/nrogowski/wows_whiteboard/model/strats/Flowchart.java
+++ src/main/java/org/comit/nrogowski/wows_whiteboard/model/strats/Flowchart.java
@@ -16,7 +16,72 @@ public abstract class Flowchart<D> {
 	
 	// Logically this class could implement set. For now this is deemed unnecessary.
 	
+	/** Origin node can be null, indicating this flowchart is empty */
+	private Node<D> origin;
 	
+	/**
+	 * Basically an Iterator, but branching-sensitive
+	 */
+	public class Navigator {
+		Node<D> position = null;
+		
+		/**
+		 * Gets the number of branches directly forward from the current node, or zero if there are none.
+		 * This is equivalent to the size of the list of nodes available to navigate to using the
+		 * {@link Navigator#next(int)} method.
+		 * @return the number of nodes immediately following the current node
+		 */
+		public int numPathsForward() {
+			if (position == null) {
+				return origin != null ? 1 : 0;
+			}
+			return position.nexts().size();
+		}
+		
+		/**
+		 * Gets the next element in the flowchart, using the provided index to choose which branch to take.
+		 * <p>
+		 * If no next elements exist, this method will return <code>null</code> instead of an error if it receives
+		 * an index of zero.
+		 * @param index A valid index for which branch to take, or zero.
+		 * Non-zero values which are out of the valid range
+		 * <br />(<code>index >= </code>{@link Navigator#numPathsForward()})
+		 * <br />will cause an exception.
+		 * @see {@link Navigator#numPathsForward()}
+		 * @return The chosen element, or <code>null</code> if there are no elements beyond this one.
+		 * @throws IndexOutOfBoundsException If index is non-zero and out of bounds for this node. 
+		 */
+		public D next(int index) {
+			D returnValue;
+			
+			// Initial call (or empty flowchart)
+			if (position == null) {
+				if (index != 0) {
+					throw new IndexOutOfBoundsException(
+						"null position cannot have multiple branches, requested index was " + index);
+				}
+				position = origin;
+				returnValue = position != null ? position.contents : null;  
+			}
+			// Subsequent calls
+			else {
+				// we specifically do not test for index >= position.nexts().size(), so the exception rises to caller
+				position = position.nexts().get(index);
+				returnValue = position.contents;
+			}
+			
+			return returnValue;
+		}
+		
+		public D prev() {
+			// null means either the flowchart is empty, or prev() was called first, either way nothing's back there 
+			position = position == null ? null : position.prev();
+			
+			// Unlike for next(), since we can only have one node behind us, we never want to throw an exception,
+			// so just return null if nothing is back there
+			return position == null ? null : position.contents;
+		}
+	}
 }
 
 
