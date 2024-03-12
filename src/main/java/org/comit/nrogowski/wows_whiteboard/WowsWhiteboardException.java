a0cf131e3306c3e1ad14f944991156904bf0f374
caffeinathan <nkrogowski+github1@gmail.com>
2024-03-12 01:12:52 -0600

    add exception children

---
+++ src/main/java/org/comit/nrogowski/wows_whiteboard/WowsWhiteboardException.java
@@ -0,0 +1,34 @@
+package org.comit.nrogowski.wows_whiteboard;
+
+public class WowsWhiteboardException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 4916964587114245003L;
+
+	public WowsWhiteboardException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WowsWhiteboardException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WowsWhiteboardException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WowsWhiteboardException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WowsWhiteboardException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
