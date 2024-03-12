75c2c309f0c42be282743d649d439c716dd861a3
caffeinathan <nkrogowski+github1@gmail.com>
2024-03-11 21:41:38 -0600

    initial spring initializr project

---
+++ src/main/java/org/comit/nrogowski/wows_whiteboard/WoWsWhiteboardApplication.java
@@ -0,0 +1,13 @@
+package org.comit.nrogowski.wows_whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoWsWhiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoWsWhiteboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/comit/nrogowski/wows_whiteboard/WoWsWhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.nrogowski.wows_whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoWsWhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
