ffcf71a738a5409b2a1df059e205f5cd19f3a5a2
caffeinathan <nkrogowski+github1@gmail.com>
2024-04-28 14:25:26 -0600

    add logical canvas dimensions

--- src/main/java/org/comit/nrogowski/wows_whiteboard/Constants.java
+++ src/main/java/org/comit/nrogowski/wows_whiteboard/Constants.java
@@ -30,4 +30,18 @@ public class Constants {
 	public static final String WG_AS_AUTH_URI_WITHOUT_BINDING =
 			String.format("%s?application_id=%s&redirect_uri=http://%s",
 			WG_AS_AUTH_PATH, WG_APPLICATION_ID, APPLICATION_DOMAIN);
+	
+	/**
+	 * All pixel values are interpreted as residing on a square canvas of this dimension.
+	 * <p>
+	 * Actual canvas size is left as a front-end implementation detail. All values passing to or from the frontend 
+	 * shall comport with this logical canvas size.
+	 * <p>
+	 * A value of 3600 has the following properties:
+	 * <ul><li>evenly divisible by 9, 25, and 16.</li>
+	 * <li>on current max map size of 48000m, resolves the space to the nearest 13.3 metres
+	 * (smallest resolvable distance greater than 1/1000th could become noticeably imprecise)</li>
+	 * <li>requires less than two bytes of memory per coordinate</li></ul>
+	 */
+	public static final int CANVAS_LOGICAL_SIDE_LENGTH = 3600;
 }
